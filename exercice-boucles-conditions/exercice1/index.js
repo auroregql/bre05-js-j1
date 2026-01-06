07b091b7798d7c5b3f28e9ce97df104d2416fb7a
auroregql <aurore.gicquel.06@gmail.com>
2026-01-06 13:24:55 +0000

    Exercice 1 terminé

---
+++ exercice-boucles-conditions/exercice1/index.js
@@ -0,0 +1,28 @@
+let i=0;
+
+while(i<10)
+{
+	console.log(i);
+	i = i + 1;
+}
+
+let j=20;
+
+while(j<25)
+{
+	console.log(j);
+	j = j + 1;
+}
+
+let k=0;
+
+while(k<20)
+{
+	if(k % 2 === 0)
+	{
+		console.log(k)
+	}
+	
+	k = k + 1
+}
+
