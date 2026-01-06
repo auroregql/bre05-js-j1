3f61c0f7725373007662897092edbdaec43395a2
auroregql <aurore.gicquel.06@gmail.com>
2026-01-06 13:45:23 +0000

    Exercice 3 Terminé

---
+++ exercice-boucles-conditions/exercice3/index.js
@@ -0,0 +1,20 @@
+let i=0;
+
+while (i<100){
+    if (i<=50) {
+        if (i % 2 === 0) {
+            console.log("Pair");
+        } else {
+            console.log("Impair");
+        }
+        i = i + 2;
+    }
+    else {
+        if (i % 2 === 0) {
+            console.log("Pair");
+        } else {
+            console.log("Impair");
+        }
+        i = i + 3;
+    }
+}
